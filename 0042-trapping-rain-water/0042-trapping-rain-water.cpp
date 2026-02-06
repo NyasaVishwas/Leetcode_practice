facae929fdbe8d2a53155b167df0019d71666b34
Nyasa Vishwas <152083045+NyasaVishwas@users.noreply.github.com>
2026-02-06 11:25:16 +0530

    Time: 1 ms (24.19%), Space: 27 MB (39.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int trap(vector<int>& h) {
         int n = h.size();
-        vector<int> prefix(n , 0), suffix(n , 0);
-        int mx = 0;
-        for (int i = 0; i < n; i++){
-            mx = max(mx, h[i]);
-            prefix[i] = mx;
-        }
-        mx = 0;
-        for (int i = n - 1; i >= 0; i--){
-            mx = max(mx, h[i]);
-            suffix[i] = mx;
-        }
+        stack<int> s;
         int ans = 0;
-        for (int i = 1; i < n - 1; i++){
-            int water = min(prefix[i], suffix[i]) - h[i];
-            if (water >= 0) ans += water;
-        }
+        for (int i = n-1; i >= 0; i--){
+            while (!s.empty() && h[s.top()] < h[i]){
+                int mid = s.top();
+                s.pop();
+                if (s.empty()) continue;
+                int left = h[i];
+                int right = h[s.top()];
+                int width = s.top() - i - 1;
+                ans += (min(left, right) - h[mid]) * width;
+            }
+            s.push(i);
+        }   
         return ans;
     }
 };
